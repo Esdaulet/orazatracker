685c20966cbc9b3a2287142907939afe15c12270
Yesdaulet <yesdauletAskar@gmail.com>
2026-02-22 22:45:10 +0500

    fix: read Firebase credentials from env on Railway

--- backend/telegram-bot/src/firebase.ts
+++ backend/telegram-bot/src/firebase.ts
@@ -3,17 +3,27 @@ import * as path from "path";
 import * as fs from "fs";
 
 if (!admin.apps.length) {
-  const serviceAccountPath = path.join(__dirname, "../../serviceAccount.json");
-  if (fs.existsSync(serviceAccountPath)) {
-    const serviceAccount = JSON.parse(fs.readFileSync(serviceAccountPath, "utf8"));
+  if (process.env.FIREBASE_SERVICE_ACCOUNT) {
+    // Production (Railway): read from env variable
+    const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
     admin.initializeApp({
       credential: admin.credential.cert(serviceAccount),
       databaseURL: "https://orazaapp-default-rtdb.firebaseio.com",
     });
   } else {
-    admin.initializeApp({
-      databaseURL: "https://orazaapp-default-rtdb.firebaseio.com",
-    });
+    // Local dev: read from file
+    const serviceAccountPath = path.join(__dirname, "../../serviceAccount.json");
+    if (fs.existsSync(serviceAccountPath)) {
+      const serviceAccount = JSON.parse(fs.readFileSync(serviceAccountPath, "utf8"));
+      admin.initializeApp({
+        credential: admin.credential.cert(serviceAccount),
+        databaseURL: "https://orazaapp-default-rtdb.firebaseio.com",
+      });
+    } else {
+      admin.initializeApp({
+        databaseURL: "https://orazaapp-default-rtdb.firebaseio.com",
+      });
+    }
   }
 }
 
